918c19b436606c68c2873f1757936dfb91194861
asgarovf <farhad@getwhelp.com>
2022-09-28 15:05:35 +0300

    chore: remove unnecesarry font load state

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,12 @@
 import 'styles/global.scss';
 import type { AppProps } from 'next/app';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { CONFIG } from 'config';
 
 function EthyleneApp({ Component, pageProps }: AppProps) {
-  const [fontLoaded, setFontLoaded] = useState(false);
-
   useEffect(() => {
     document.body.style.fontFamily = CONFIG.FONT_FAMILY;
-    setFontLoaded(true);
   }, []);
-
-  if (!fontLoaded) {
-    return null;
-  }
 
   return <Component {...pageProps} />;
 }
---
+++ pages/demo/components.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from 'next';
+
+const Components: NextPage = () => {
+  return <div>components</div>;
+};
+
+export default Components;
