2e4ac3bac27617cd84844b3a253a7378940cb146
Rafi Ersözlü <ersozlu.rafi@gmail.com>
2023-07-15 16:11:45 +0300

    refactor: tests are more descriptive and toEqual changed toBe to check types as well

--- v2/utils/toHexString.test.ts
+++ v2/utils/toHexString.test.ts
@@ -2,11 +2,13 @@ import { expect, test } from '@jest/globals';
 
 import { toHexString } from './toHexString';
 
-test('adds 0x to normal string', () => {
-    expect(toHexString('alim')).toEqual('0xalim');
-});
+describe('when toHexString is called, it', () => {
+    it('should add 0x to a string without 0x prefix', () => {
+        expect(toHexString('alim')).toBe('0xalim');
+    });
 
-test('return input if it starts with 0x', () => {
-    const input = '0xalim';
-    expect(toHexString(input)).toEqual(input);
+    it('should return string as it is, if it starts with 0x', () => {
+        const input = '0xalim';
+        expect(toHexString(input)).toBe(input);
+    });
 });
