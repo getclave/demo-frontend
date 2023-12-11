5342a3951ad956c16a04df85f9d5178c39dd5a17
aalimsahin <alimsahin0007@gmail.com>
2023-12-11 04:30:41 +0300

    feat: updated all ui

---
+++ components/ZkEmailRecovery/Onboarding/Create/Create.tsx
@@ -0,0 +1,242 @@
+import { useMutation } from '@tanstack/react-query';
+import type { AxiosResponse } from 'axios';
+import { useDebounce } from 'hooks';
+import { useNotify } from 'hooks';
+import { useGetCreate2Address } from 'hooks/useGetCreate2Address';
+import type { ModalController } from 'hooks/useModal';
+import { authenticate, register } from 'module/webauthn';
+import { getInitChallenge, sendInitUserOp } from 'module/webauthn';
+import { encodeChallenge, getPublicKey } from 'module/webauthnUtils';
+import { useGetAccountQueryV2 } from 'queries/useGetAccountQueryV2';
+import { useEffect, useState } from 'react';
+import { IoIosArrowBack } from 'react-icons/io';
+import { useDispatch, useSelector } from 'react-redux';
+import { apiCreateAccountV2 } from 'restapi';
+import type { AccountV2, CreateAccountDto } from 'restapi/types';
+import type { RootState } from 'store';
+import {
+    setZKAccount,
+    setZKAuthenticationResponse,
+    setZKDeployedContractAddress,
+    setZKRegistrationResponse,
+    setZKSelectedAccount,
+} from 'store/slicers/zkaccount';
+import { setZKConnectionOption } from 'store/slicers/zkconnection';
+import { ConnectionOptions } from 'types/connection';
+import { Button, Input } from 'ui';
+
+import styles from './Create.module.scss';
+
+export function Create({
+    infoModal,
+    setInfo,
+    accountName,
+    setAccountName,
+}: {
+    infoModal: ModalController;
+    setInfo: (value: string) => void;
+    accountName: string;
+    setAccountName: (accountName: string) => void;
+}): JSX.Element {
+    const notify = useNotify();
+    const dispatch = useDispatch();
+    const [errorMessage, setErrorMessage] = useState<string>('');
+    const [loading, setLoading] = useState<boolean>(false);
+    const connectionOption = useSelector(
+        (state: RootState) => state.zkconnection.connectionOption,
+    );
+
+    const debounced = useDebounce(accountName, 500);
+    const { data, isLoading } = useGetAccountQueryV2(debounced);
+
+    const { mutate: postAccount } = useMutation({
+        mutationFn: async (
+            params: CreateAccountDto,
+        ): Promise<AxiosResponse<AccountV2>> => apiCreateAccountV2(params),
+        onError: (e) => {
+            setLoading(false);
+            infoModal.close();
+            notify.error('Account could not be created!');
+            console.log(e);
+        },
+        onSuccess: (data) => {
+            setLoading(false);
+            notify.success(`Account created successfully <3`);
+            dispatch(setZKAccount(data.data));
+            setInfo('CREATEDWALLET');
+            setTimeout(() => {
+                infoModal.close();
+            }, 5000);
+        },
+    });
+
+    const handleRegister = async (): Promise<void> => {
+        if (accountName === '') return;
+        if (errorMessage !== '') return;
+        try {
+            setInfo('CREATEREGISTER');
+            infoModal.open();
+            const registrationResponse = await register(`${accountName}`);
+            if (registrationResponse) {
+                setInfo('CREATEAUTH');
+                setLoading(true);
+                dispatch(setZKRegistrationResponse(registrationResponse));
+                try {
+                    const publicKey: string = await getPublicKey(
+                        registrationResponse?.credential.publicKey,
+                    );
+
+                    const create2Address: string = await useGetCreate2Address(
+                        publicKey,
+                    );
+
+                    const challenge = await getInitChallenge(
+                        create2Address,
+                        publicKey,
+                    );
+                    const encodedChallenge = encodeChallenge(challenge);
+
+                    const authenticationResponse = await authenticate(
+                        [registrationResponse.credential.id],
+                        encodedChallenge,
+                    );
+
+                    dispatch(
+                        setZKAuthenticationResponse(authenticationResponse),
+                    );
+                    if (authenticationResponse) {
+                        setInfo('TXSENT');
+                        const res = await sendInitUserOp(
+                            challenge,
+                            publicKey,
+                            encodedChallenge,
+                            authenticationResponse.signature,
+                            authenticationResponse.authenticatorData,
+                            authenticationResponse.clientData,
+                            create2Address,
+                        );
+
+                        if (res) {
+                            const lStorage =
+                                localStorage.getItem('ClaveAccounts');
+                            const accountsFromLS = lStorage
+                                ? JSON.parse(lStorage)
+                                : [];
+                            console.log(lStorage, accountsFromLS);
+                            accountsFromLS.push(accountName);
+                            localStorage.setItem(
+                                'ClaveAccounts',
+                                JSON.stringify(accountsFromLS),
+                            );
+                            const ClaveAccount = {
+                                account: {
+                                    id: 0,
+                                    name: accountName,
+                                    address: create2Address,
+                                    options: [
+                                        {
+                                            id: 0,
+                                            method_name: `${accountName}`,
+                                            public_key: publicKey,
+                                            type: 1,
+                                            client_id:
+                                                registrationResponse.credential
+                                                    .id,
+                                        },
+                                    ],
+                                },
+                                selectedAccount: 0,
+                            };
+                            localStorage.setItem(
+                                'ClaveAccount',
+                                JSON.stringify(ClaveAccount),
+                            );
+                            setLoading(false);
+                            dispatch(
+                                setZKDeployedContractAddress(create2Address),
+                            );
+                            dispatch(setZKSelectedAccount(0));
+                            postAccount({
+                                name: accountName,
+                                address: create2Address,
+                                authName: `${accountName}`,
+                                authPublic: publicKey,
+                                authType: 1,
+                                clientId: registrationResponse.credential.id,
+                            } as CreateAccountDto);
+                        }
+                    }
+                } catch (e) {
+                    console.log(e);
+                }
+            }
+        } catch (e) {
+            console.log(e);
+            infoModal.close();
+            setLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        if (data) {
+            setErrorMessage('This username is already taken!');
+        }
+    }, [data]);
+
+    useEffect(() => {
+        setErrorMessage('');
+    }, [connectionOption]);
+
+    useEffect(() => {
+        setLoading(false);
+        setErrorMessage('');
+    }, []);
+
+    return (
+        <div className={styles.wrapper}>
+            <div
+                className={styles.back}
+                onClick={(): void => {
+                    dispatch(setZKConnectionOption(ConnectionOptions.CONNECT));
+                }}
+            >
+                <IoIosArrowBack size={20} />
+            </div>
+            <div className={styles.title}>
+                <span className={styles.text}>Create New Account</span>
+            </div>
+            <div className={styles.nickname}>
+                <Input
+                    color="special"
+                    placeholder="Username"
+                    height="40px"
+                    width={'100%'}
+                    error={errorMessage}
+                    value={accountName}
+                    onChange={(e): void => {
+                        setAccountName(e.target.value);
+                        setErrorMessage('');
+                    }}
+                />
+            </div>
+            <div className={styles.button}>
+                <Button
+                    disabled={
+                        accountName === '' || isLoading || errorMessage !== ''
+                    }
+                    width="120px"
+                    height="40px"
+                    color="special"
+                    loading={
+                        !accountName === null ? isLoading : false || loading
+                    }
+                    onClick={async (): Promise<void> => {
+                        await handleRegister();
+                    }}
+                >
+                    Register
+                </Button>
+            </div>
+        </div>
+    );
+}
---
+++ components/ZkEmailRecovery/Onboarding/Select/Select.tsx
@@ -0,0 +1,256 @@
+import { useNotify } from 'hooks';
+import type { ModalController } from 'hooks/useModal';
+import { useResetAllStore } from 'hooks/useResetStore';
+import { useVerifyAuthentication } from 'hooks/useVerifyAuthentication';
+import { register } from 'module/webauthn';
+import { getPublicKey } from 'module/webauthnUtils';
+import { QRCodeSVG } from 'qrcode.react';
+import { useGetAccountQueryV2 } from 'queries/useGetAccountQueryV2';
+import { useEffect, useState } from 'react';
+import { browserName } from 'react-device-detect';
+import { AiOutlineLaptop } from 'react-icons/ai';
+import { IoIosArrowBack } from 'react-icons/io';
+import { TbDeviceMobile } from 'react-icons/tb';
+import { useDispatch, useSelector } from 'react-redux';
+import type { Option } from 'restapi/types';
+import { Authenticator } from 'restapi/types';
+import type { RootState } from 'store';
+import {
+    setZKAccount,
+    setZKRegistrationResponse,
+    setZKSelectedAccount,
+} from 'store/slicers/zkaccount';
+import { Button } from 'ui';
+import { clsnm } from 'utils/clsnm';
+import { isInArray } from 'utils/isInArray';
+
+import styles from './Select.module.scss';
+
+export function Select({
+    infoModal,
+    setInfo,
+}: {
+    infoModal: ModalController;
+    setInfo: (value: string) => void;
+}): JSX.Element {
+    const notify = useNotify();
+    const dispatch = useDispatch();
+    const [selectOrCreate, setSelectOrCreate] = useState<boolean>(false);
+    const [publicKey, setPublicKey] = useState<string | null>(null);
+    const account = useSelector((state: RootState) => state.zkaccount.account);
+    const selectedAccount = useSelector(
+        (state: RootState) => state.zkaccount.selectedAccount,
+    );
+    const [lStorage, setLStorage] = useState<Array<string>>([]);
+    const registrationResponse = useSelector(
+        (state: RootState) => state.zkaccount.registrationResponse,
+    );
+    const [previousOptions, setPreviousOptions] = useState<Array<Option>>([]);
+    const { zkResetAllStore } = useResetAllStore();
+    const { data } = useGetAccountQueryV2(
+        account ? account?.name : '',
+        !publicKey ? false : true,
+    );
+
+    const handleVerifyAuthentication = async (
+        option: Option,
+        _index: number,
+    ): Promise<void> => {
+        if (!account) return;
+        if (option.type !== Authenticator.DESKTOP) return;
+        try {
+            const clientId = account.options[_index]
+                ? account.options[_index]?.client_id
+                : '';
+            if (!clientId) return;
+            const result = await useVerifyAuthentication(
+                account,
+                clientId,
+                account.options[_index].public_key,
+                infoModal,
+                setInfo,
+            );
+            if (result) {
+                dispatch(setZKSelectedAccount(_index));
+                const ClaveAccount = {
+                    account: account,
+                    selectedAccount: _index,
+                };
+                localStorage.setItem(
+                    'ClaveAccount',
+                    JSON.stringify(ClaveAccount),
+                );
+            }
+        } catch (e) {
+            console.log(e);
+        }
+    };
+
+    const handleRegister = async (): Promise<void> => {
+        try {
+            const registrationResponse = await register(
+                `${account?.name}-${browserName}-${
+                    account?.options ? account?.options.length + 1 : '9'
+                }`,
+            );
+            if (registrationResponse) {
+                dispatch(setZKRegistrationResponse(registrationResponse));
+                const publicKey: string = await getPublicKey(
+                    registrationResponse?.credential.publicKey,
+                );
+                setPublicKey(publicKey);
+
+                setSelectOrCreate(true);
+                if (account) {
+                    dispatch(setZKSelectedAccount(account?.options.length + 1));
+                }
+            }
+        } catch (e) {
+            console.log(e);
+            notify.error(e as string);
+        }
+    };
+
+    useEffect(() => {
+        if (previousOptions.length === 0) {
+            if (data) {
+                setPreviousOptions(data.data.options);
+            }
+        } else {
+            if (data) {
+                if (data.data.options.length > previousOptions.length) {
+                    setPreviousOptions(data.data.options);
+                    setInfo('AUTHED');
+                    infoModal.open();
+                    setTimeout(() => {
+                        infoModal.close();
+                    }, 3000);
+                    dispatch(setZKAccount(data.data));
+                    dispatch(
+                        setZKSelectedAccount(data.data.options.length - 1),
+                    );
+
+                    const lStorage = localStorage.getItem('ClaveAccounts');
+                    const accountsFromLS = lStorage ? JSON.parse(lStorage) : [];
+                    accountsFromLS.push(account?.name);
+                    localStorage.setItem(
+                        'ClaveAccounts',
+                        JSON.stringify(accountsFromLS),
+                    );
+                    const ClaveAccount = {
+                        account: data.data,
+                        selectedAccount: data.data.options.length - 1,
+                    };
+                    localStorage.setItem(
+                        'ClaveAccount',
+                        JSON.stringify(ClaveAccount),
+                    );
+                }
+            }
+        }
+    }, [data]);
+
+    useEffect(() => {
+        const localS = localStorage.getItem('ClaveAccounts');
+        setLStorage(localS ? JSON.parse(localS) : lStorage);
+    }, []);
+
+    return (
+        <div className={styles.wrapper}>
+            <div
+                className={styles.back}
+                onClick={(): void => {
+                    if (publicKey && selectOrCreate) {
+                        setPublicKey(null);
+                        setSelectOrCreate(false);
+                    } else if (selectOrCreate) {
+                        setSelectOrCreate(false);
+                    } else {
+                        console.log(selectedAccount);
+                        zkResetAllStore();
+                    }
+                }}
+            >
+                <IoIosArrowBack size={20} color="white" />
+            </div>
+            <div className={styles.title}>
+                <span className={styles.text}>
+                    {!selectOrCreate
+                        ? 'Select Account'
+                        : publicKey
+                        ? 'Scan QR Code on Clave Mobile'
+                        : 'Register to create new PublicKey'}
+                </span>
+            </div>
+            {!selectOrCreate ? (
+                <div className={styles.accounts}>
+                    {account &&
+                        account.options.map((option: Option, i: number) => {
+                            return (
+                                <div
+                                    className={
+                                        option.type === Authenticator.DESKTOP
+                                            ? styles.account
+                                            : clsnm(styles.account, styles.off)
+                                    }
+                                    key={i}
+                                    onClick={async (): Promise<void> => {
+                                        await handleVerifyAuthentication(
+                                            option,
+                                            i,
+                                        );
+                                    }}
+                                >
+                                    <div className={styles.name}>
+                                        {option?.method_name}
+                                    </div>
+                                    <div className={styles.device}>
+                                        {option.type ===
+                                        Authenticator.DESKTOP ? (
+                                            <AiOutlineLaptop />
+                                        ) : (
+                                            <TbDeviceMobile />
+                                        )}
+                                    </div>
+                                </div>
+                            );
+                        })}
+                    {account && !isInArray(lStorage, account.name) && (
+                        <div className={styles.newAccount}>
+                            <Button
+                                width="215px"
+                                height="30px"
+                                color="purple"
+                                fontSize="fs14"
+                                fontWeight="fw400"
+                                onClick={async (): Promise<void> => {
+                                    await handleRegister();
+                                }}
+                            >
+                                Authenticate this device
+                            </Button>
+                        </div>
+                    )}
+                </div>
+            ) : !publicKey ? null : (
+                <div className={styles.qrCode}>
+                    {publicKey && (
+                        <QRCodeSVG
+                            value={JSON.stringify({
+                                name: account?.name,
+                                publicKey: publicKey,
+                                authName: `${account?.name}-${browserName}-${
+                                    account?.options
+                                        ? account?.options.length + 1
+                                        : publicKey.slice(2, 4)
+                                }`,
+                                clientId: registrationResponse?.credential.id,
+                            })}
+                            size={250}
+                        />
+                    )}
+                </div>
+            )}
+        </div>
+    );
+}
